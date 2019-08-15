ec7580dd75c144edc9a0491fbcf03026f617c48a
Philipp Hinz <phisn@outlook.de>
2019-08-15 12:51:00 +0200

    Dir Restart

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,29 +1,9 @@
-#include "Host.h"
-#include "Visual.h"
-
-#include <ESP8266WiFi.h>
-
 void setup()
 {
-	Serial.begin(115200);
-	Serial.println();
 
-	Serial.print("Setting soft-AP ... ");
-	boolean result = WiFi.softAP(
-		NETWORK_NAME,
-		NETWORK_PASS);
-	if (result == true)
-	{
-		Serial.println("Ready");
-	}
-	else
-	{
-		Serial.println("Failed!");
-	}
 }
 
 void loop()
 {
-	Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
-	delay(3000);
+
 }
